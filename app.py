2e925f744a23c53d756d5d0903e0205722951d04
petejmoore-ui <pete.j.moore@gmail.com>
2026-03-28 09:24:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1808,6 +1808,13 @@ BASE = """<!DOCTYPE html>
   gtag('js', new Date());
   gtag('config', 'G-TBH27VXH8M');
 </script>
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "w2re1o5146");
+</script>
 </head>
 <body>
 
